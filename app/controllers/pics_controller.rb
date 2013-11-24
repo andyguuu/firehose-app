0d1a69b7690b8b0df78ef4afe9feef9d109f092a
Andy Gu <andy@andy-VirtualBox.(none)>
2013-11-24 12:18:36 -0500

    added edit feature from show page

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,28 @@
 class PicsController < ApplicationController
 
 
+	def edit
+		@pic=Pic.find (params[:id])	
+	end
+
+
+
+	def update
+		@pic=Pic.find (params[:id])	
+
+		@pic.update_attributes( pic_params )
+
+		if @pic.valid?
+			redirect_to pics_path 
+		else
+			render :edit, :status => :unprocessable_entity
+		end	
+
+	end		
+
+
+
+
 	def show
 		@pic=Pic.find (params[:id])
 	end
